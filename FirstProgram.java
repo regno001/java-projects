94301e497d5c07820ae4ebbbc40a6d53c82e392d
regno001 <rwt7454@gmail.com>
2025-07-29 22:12:07 +0530

    Update FirstProgram.java

--- FirstProgram.java
+++ FirstProgram.java
@@ -3,5 +3,7 @@ import java.lang.*;
 public class FirstProgram {
     public static void main(String[] args) {
         System.out.println("Hello World");
+          System.out.println("welcome ");
+          System.out.println("to Regno github");
     }
 }
