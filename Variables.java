4f93b3f936371da40cfb687fc2297dac82471b9a
regno001 <rwt7454@gmail.com>
2025-08-01 23:02:30 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myNumber = 865;
+
+        int yourNumber ;
+        yourNumber=865;
+        System.out.println(yourNumber);
+        yourNumber=45;
+        System.out.println(yourNumber);
+    }
+}
