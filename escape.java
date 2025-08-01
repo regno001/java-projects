0f0b1ea5d77dad0a908e7a0b73e5aea519365788
regno001 <rwt7454@gmail.com>
2025-08-01 23:15:51 +0530

    Add files via upload

---
+++ escape.java
@@ -0,0 +1,7 @@
+public class escape {
+    public static void main(String[] args) {
+        System.out.println("Hello \"Regno\"");
+        System.out.println("Hello \\Regno\\");
+        System.out.println("Hello \nRegno \nWelcome \nto THE \nJava World");
+    }
+}
