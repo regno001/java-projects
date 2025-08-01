8aa7975946ec19feb07d5967e8ed43d68624efbe
regno001 <rwt7454@gmail.com>
2025-08-01 23:36:11 +0530

    Add files via upload

---
+++ addUserInput.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class addUserInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter The First Number");
+        int a= input.nextInt();
+        System.out.println("Enter the second Number");
+    int b= input.nextInt();
+    int c=a+b;
+        System.out.println("The ANswer is"+c);
+
+    }
+}
