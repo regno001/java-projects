8e0677d9cad25b24c224bbbbc90235fc864f022d
regno001 <rwt7454@gmail.com>
2025-08-02 22:49:12 +0530

    Add files via upload

---
+++ arithmatic.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class arithmatic {
+    public static void main(String[] args) {
+        Scanner Input = new Scanner(System.in);
+        System.out.println("Enter the NUmber a");
+        int a = Input.nextInt();
+        System.out.println("Enter the no b ");
+        int b = Input.nextInt();
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        double x =5;
+        double y =2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
