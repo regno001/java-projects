704b36c71d5454cc70864e79370ce8b8520d2eb7
regno001 <rwt7454@gmail.com>
2025-08-06 22:17:12 +0530

    Add files via upload

---
+++ twodimentionalArray.java
@@ -0,0 +1,19 @@
+public class twodimentionalArray {
+    public static void main(String[] args) {
+        int[][] arr={{1,2,3},{4,5,6},{7,8,9}};
+
+       int i =0;
+       while(i<arr.length){
+           int j=0;
+           while (j<arr.length){
+               System.out.print(arr[i][j]+" ");
+               j++;
+
+           }
+           i++;
+           System.out.println();
+
+       }
+    int [][] myarr=new int[2][4];
+    }
+}
