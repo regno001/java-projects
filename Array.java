fd9f224d37da05b41bf8d0fdc23bcaa403aace68
regno001 <rwt7454@gmail.com>
2025-08-06 20:51:54 +0530

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,28 @@
+public class Array {
+    public static void main(String[] args) {
+        int[]  marks = new int[8];
+        marks[0]=90;
+        marks[1]=23;
+        marks[3]=54;
+        marks[4]=965;
+        marks[5]=905;
+        marks[6]=9430;
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+        System.out.println(marks[3]);
+        System.out.println(marks[4]);
+        System.out.println(marks[5]);
+        int[] myArr = {1,2,3,4,5,6,7};
+//        System.out.println(myArr[1]);
+//        System.out.println(myArr[2]);
+//        System.out.println(myArr[3]);
+//        System.out.println(myArr[4]);
+//
+int index =0;
+while(index<5){
+    index++;
+    System.out.println(myArr[index]);
+}
+    }
+}
