a8f6b4acaf2fcb3138ff02dd3171542ddd92364e
regno001 <rwt7454@gmail.com>
2025-09-11 23:13:49 +0530

    Add files via upload

---
+++ packages.java
@@ -0,0 +1,11 @@
+package in.rahul;
+
+import java.util.Scanner;
+
+ class packages {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        in.rahulSet.packages packages= new in.rahulSet.packages();
+
+    }
+}
