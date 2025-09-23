e5567950688f74932806f30782d1a23ce21a9ce0
regno001 <rwt7454@gmail.com>
2025-09-23 21:29:37 +0530

    Add files via upload

--- car.java
+++ car.java
@@ -1,34 +1,26 @@
+package in.rahul;
+
 public class car {
-    int noOfWheels;
-    int NoOfDoors;
-    int maxSpeed;
-    String name;
-    String modelNumber;
-    String company;
-
-    public car(String modelNumber, int noOfWheels, int noOfDoors, int maxSpeed, String name, String company) {
-        this.modelNumber = modelNumber;
-        this.noOfWheels = noOfWheels;
-        NoOfDoors = noOfDoors;
-        this.maxSpeed = maxSpeed;
-        this.name = name;
-        this.company = company;
-    }
-
     @Override
     public String toString() {
         return "car{" +
-                "noOfWheels=" + noOfWheels +
-                ", NoOfDoors=" + NoOfDoors +
-                ", maxSpeed=" + maxSpeed +
-                ", name='" + name + '\'' +
-                ", modelNumber='" + modelNumber + '\'' +
-                ", company='" + company + '\'' +
+                "color='" + color + '\'' +
+                ", model='" + model + '\'' +
+                ", Fuellevel=" + Fuellevel +
+                ", costOfPurchase=" + costOfPurchase +
                 '}';
     }
 
-    public static void main(String[] args) {
-        car Swift = new car("S84",4,4,120,"Swift", "Maruti" );
-        System.out.println(Swift.toString());
+    public car(String color, String model, long costOfPurchase, double fuellevel) {
+        this.color = color;
+        this.model = model;
+        this.costOfPurchase = costOfPurchase;
+        Fuellevel = fuellevel;
     }
+
+    public String color;
+    public String model;
+    private double Fuellevel;
+     long costOfPurchase;
+
 }
