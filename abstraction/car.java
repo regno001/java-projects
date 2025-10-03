b2a7d3583f24169a7461ea71ef4a704c0580eefe
regno001 <rwt7454@gmail.com>
2025-10-03 21:32:14 +0530

    Add files via upload

---
+++ abstraction/car.java
@@ -0,0 +1,9 @@
+package in.regno.abstraction;
+
+public class car extends vehicle{
+    private int noofDoors;
+    public car() {
+
+        super(4);
+    }
+}
---
+++ abstraction/testAbstract.java
@@ -0,0 +1,7 @@
+package in.regno.abstraction;
+
+public class testAbstract {
+    public static void main(String[] args) {
+        car cr = new car();
+    }
+}
