741b6aeb257a2e947213789b0722b1d7c45ae316
regno001 <rwt7454@gmail.com>
2025-10-11 23:08:05 +0530

    Add files via upload

--- exception/calculator.java
+++ exception/calculator.java
@@ -4,18 +4,34 @@ import java.util.Scanner;
 
 public class calculator {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
+        a();
+
+    }
+    private static void a(){
+        b();
+    }
+    private static void b(){
+        c();
+    }
+    private static void c(){
+        d();
+    }
+    private static void d(){        Scanner input = new Scanner(System.in);
         System.out.println("Welcome to division Calculator");
         System.out.println("Please enter your two number");
         int a =input.nextInt();
         int b= input.nextInt();
-        try{
-            int c = a/b;
-            System.out.printf("%d result ",c);
-        }catch (ArithmeticException e) {
-            System.out.printf("%s ,Enter valid value",e.getMessage());
-        }
-
-    }
-
+        try {
+            int c = a / b;
+            System.out.printf("%d result ", c);
+//        }catch (ArithmeticException e) {
+//            System.out.printf("%s ,Enter valid value",e.getMessage());
+//        }catch(ArrayIndexOutOfBoundsException e){
+//            System.out.println("out of bound array");
+//        }
+        }catch (Throwable th){
+            System.out.println(th.getMessage());
+            throw th;
+        }}
 }
