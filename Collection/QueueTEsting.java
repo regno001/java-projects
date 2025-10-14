d91e3a6f41658426e272058587cd37134d155f8c
regno001 <rwt7454@gmail.com>
2025-10-14 23:10:01 +0530

    Add files via upload

---
+++ Collection/QueueTEsting.java
@@ -0,0 +1,21 @@
+package in.regno.exception.Collection;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueTEsting {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+         q.add(1);
+         q.offer(2);
+
+         utility.print(q);
+        System.out.println(q.peek());
+        System.out.println(q.element());
+        System.out.println(q.remove());
+        utility.print(q);
+        System.out.println(q.poll());
+
+        System.out.println(q.peek());
+    }
+}
