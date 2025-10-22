ffd1fc5a6e09a4ee213a1df8ad9a11878a6a81fa
regno001 <rwt7454@gmail.com>
2025-10-22 22:44:28 +0530

    Add files via upload

--- Thread/Runnable/TestingRunnable.java
+++ Thread/Runnable/TestingRunnable.java
@@ -9,8 +9,16 @@ public class TestingRunnable {
 
         Thread t1 = new Thread(p1);
         Thread t2 = new Thread(p2);
+        Thread t3 = new Thread(p3);
+        Thread t4 = new Thread(p4);
+        t1.setPriority(Thread.MAX_PRIORITY);
+        t2.setPriority(2);
+        t3.setPriority(3);
+        t4.setPriority(4);
         t1.start();
         t2.start();
+        t3.start();
+        t4.start();
         
     }
 }
